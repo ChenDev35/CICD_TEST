a5c3397205d18746e4db50704c60755ae94ae990
Chen-Private <chenasulin25@gmail.com>
2023-03-22 00:40:32 +0200

    update tests import

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
     res.send('<h1> Hello Worldi </h1>');
 });
 
+app.listen(port, () => {
+    console.log(`server is up and running on port: ${port}`);
+});
+
 app.get('/apple', (req, res) => {
     console.log('an apple request has arrived');
     res.send('<h1> Apple Page </h1>');
@@ -16,8 +20,4 @@ app.get('/apple', (req, res) => {
 app.get('/profile', (req, res) => {
     console.log('an apple request has arrived');
     res.send('<h1> Profile Page </h1>');
-});
-
-app.listen(port, () => {
-    console.log(`server is up and running on port: ${port}`);
 });
